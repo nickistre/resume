10e99618d555530583a6ad1f09b2d12a6b769c6a
Nicholas Istre <nickistre@gmail.com>
2014-07-13 18:35:48 -0500

    Started separating into differenct controllers.

--- js/controllers.js
+++ js/controllers.js
@@ -6,10 +6,16 @@ console.log('Loading controllers.js');
 
 var resumeControllers = angular.module('resumeControllers', []);
 
-resumeControllers.controller('resumeDataCtrl', function ($scope, $http) {
+resumeControllers.controller('bodyCtrl', function ($scope, $http) {
     $http.get('data.json').success(function(data) {
         $scope.resume = data;
     });
 
     $scope.current_date = Date.now();
+});
+
+resumeControllers.controller('headerCtrl', function ($scope, $http) {
+    $http.get('data.json').success(function (data) {
+        $scope.name = data.personal.name;
+    });
 });
